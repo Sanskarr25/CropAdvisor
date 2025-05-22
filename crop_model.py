a63771dfbd559a4f08de202bbdb55345579643f0
Sanskarr25 <sanskarshete225@gmail.com>
2025-05-22 15:01:10 +0530

    added joblib in place of pickle and generated new Crop_Recommendation.pkl file

--- crop_model.py
+++ crop_model.py
@@ -6,44 +6,44 @@ from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import model_selection
+import joblib  # <-- Import joblib
 
+# Load data
 crop = pd.read_csv('Data/crop_recommendation.csv')
-X = crop.iloc[:,:-1].values
-Y = crop.iloc[:,-1].values
+X = crop.iloc[:, :-1].values
+Y = crop.iloc[:, -1].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.15)
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.15)
 
-models = []
-models.append(('SVC', SVC(gamma ='auto', probability = True)))
-models.append(('svm1', SVC(probability=True, kernel='poly', degree=1)))
-models.append(('svm2', SVC(probability=True, kernel='poly', degree=2)))
-models.append(('svm3', SVC(probability=True, kernel='poly', degree=3)))
-models.append(('svm4', SVC(probability=True, kernel='poly', degree=4)))
-models.append(('svm5', SVC(probability=True, kernel='poly', degree=5)))
-models.append(('rf',RandomForestClassifier(n_estimators = 21)))
-models.append(('gnb',GaussianNB()))
-models.append(('knn1', KNeighborsClassifier(n_neighbors=1)))
-models.append(('knn3', KNeighborsClassifier(n_neighbors=3)))
-models.append(('knn5', KNeighborsClassifier(n_neighbors=5)))
-models.append(('knn7', KNeighborsClassifier(n_neighbors=7)))
-models.append(('knn9', KNeighborsClassifier(n_neighbors=9)))
+# Define models
+models = [
+    ('SVC', SVC(gamma='auto', probability=True)),
+    ('svm1', SVC(probability=True, kernel='poly', degree=1)),
+    ('svm2', SVC(probability=True, kernel='poly', degree=2)),
+    ('svm3', SVC(probability=True, kernel='poly', degree=3)),
+    ('svm4', SVC(probability=True, kernel='poly', degree=4)),
+    ('svm5', SVC(probability=True, kernel='poly', degree=5)),
+    ('rf', RandomForestClassifier(n_estimators=21)),
+    ('gnb', GaussianNB()),
+    ('knn1', KNeighborsClassifier(n_neighbors=1)),
+    ('knn3', KNeighborsClassifier(n_neighbors=3)),
+    ('knn5', KNeighborsClassifier(n_neighbors=5)),
+    ('knn7', KNeighborsClassifier(n_neighbors=7)),
+    ('knn9', KNeighborsClassifier(n_neighbors=9)),
+]
 
+# Voting classifier
 vot_soft = VotingClassifier(estimators=models, voting='soft')
 vot_soft.fit(X_train, y_train)
 y_pred = vot_soft.predict(X_test)
 
-scores = model_selection.cross_val_score(vot_soft, X_test, y_test,cv=5,scoring='accuracy')
-print("Accuracy: ",scores.mean())
+# Cross-validation and accuracy
+scores = model_selection.cross_val_score(vot_soft, X_test, y_test, cv=5, scoring='accuracy')
+print("Accuracy: ", scores.mean())
 
 score = accuracy_score(y_test, y_pred)
 print("Voting Score % d" % score)
 
-# import pickle
-# pkl_filename = 'Crop_Recommendation.pkl'
-# Model_pkl = open(pkl_filename, 'wb')
-# pickle.dump(vot_soft, Model_pkl)
-# Model_pkl.close()
-
-import pickle
-with open('Crop_Recommendation.pkl', 'wb') as f:
-    pickle.dump(vot_soft, f)
+# Save model using joblib
+joblib.dump(vot_soft, 'Crop_Recommendation.pkl')
